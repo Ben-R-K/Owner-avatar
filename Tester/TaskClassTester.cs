d24577e52c0c50b334ad94425fb27d26f84e0e08
Ben Krüger <benx0071@edu.sde.dk>
2023-08-09 13:14:29 +0200

    Deleted old unittest project and created a new. The reson is the old was in .net 4.72 witch is incompareble with .net 6.0.

---
+++ Tester/TaskClassTester.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tester
+{
+    public class TaskClassTester
+    {
+        [Fact]
+        public void NumberSelecterSchuldReturnBiggest()
+        {
+            //arange
+            int[] testints = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int biggestint = testints[0];
+            foreach (int i in testints)
+            {
+                if (i > biggestint)
+                {
+                    biggestint = i;
+                }
+            }
+            int Expected = biggestint;
+            //act
+
+            //assert
+        }
+    }
